60034e225b8634d81b0a956e5fba08c062dd2f72
Ulyana Dyakova <ulyanadyakova@gmail.com>
2020-02-13 19:02:21 +0300

    need to add -d key

---
+++ track_uninstalled_modules.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+
+def collect_imports(directory=os.getcwd()):
+    """
+    Allows to get all imports from scripts in the specified directory
+    :param directory: full path
+    :return: list of imports (no duplicates)
+    """
+    folder = [file for file in os.walk(directory)]
+    imports = []
+    for address, dirs, files in folder:
+        for file in files:
+            if file.endswith('.py'):
+                with open(address + '/' + file, 'r', encoding='utf-8-sig') as f:
+                    strings = f.read().splitlines()
+                for string in strings:
+                    if string.startswith('import '):
+                        imports.append(string.replace('import ', ''))
+                    elif string.startswith('from'):
+                        imports.append(string.split(' ')[1])
+    return list(set(imports))
+
+
+def collect_uninstalled_modules(imports: list, not_installed=None):
+    """
+    Displays a list of uninstalled modules
+    :param imports: list of all imports
+    :param not_installed: list for uninstalled modules
+    :return: list of uninstalled modules
+    """
+    if not_installed is None:
+        not_installed = []
+    for imp in imports:
+        try:
+            __import__(imp)
+            # print(f'{str(imp)}: Module was installed')
+        except ModuleNotFoundError:
+            # print(f'{str(imp)}: There was no such module installed')
+            not_installed.append(imp)
+    return not_installed
+
+
+def is_installed(imports: list):
+    """
+    Raises an ModuleNotFoundError if a module is not installed
+    :param imports: list of all imports
+    :return: OK if all imports is installed (or raises an error)
+    """
+    for imp in imports:
+        try:
+            __import__(imp)
+            # print(f'{str(imp)}: Module was installed')
+        except ModuleNotFoundError:
+            sys.tracebacklimit = 0
+            raise
+    return print('OK')
+
+
+print(collect_uninstalled_modules(collect_imports()))
+print(is_installed(collect_imports()))
